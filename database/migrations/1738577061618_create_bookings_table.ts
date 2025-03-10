7dc4517173ca352cf700752cd58e42f51eb5ffad
Romain Boulland <romain.boulland@capgemini.com>
2025-03-10 11:48:54 +0100

    Simplification script migration bdd

--- database/migrations/1738577061618_create_bookings_table.ts
+++ database/migrations/1738577061618_create_bookings_table.ts
@@ -1,20 +1,30 @@
-import { BaseSchema } from '@adonisjs/lucid/schema'
+import { BaseSchema } from "@adonisjs/lucid/schema";
 
 export default class extends BaseSchema {
-  protected tableName = 'bookings'
+  protected tableName = "bookings";
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.integer('desktopId').unsigned().references('desktops.id').onDelete('CASCADE') // delete booking when buro is deleted
-      table.timestamp('date')
-      table.string('email')
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
-    })
+      table.increments("id");
+      table
+        .integer("desktopId")
+        .unsigned()
+        .references("desktops.id")
+        .onDelete("CASCADE"); // delete booking when buro is deleted
+      table.date("date");
+      table.timestamp("created_at");
+      table.timestamp("updated_at");
+      table
+        .integer("userId")
+        .unsigned()
+        .references("users.id")
+        .onDelete("CASCADE");
+      table.unique(["desktopId", "date"]);
+      table.unique(["userId", "date"]);
+    });
   }
 
   async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.dropTable(this.tableName);
   }
 }
